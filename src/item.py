afb2381f24fc88cd5bd62c7624f4aaace8cbe5cd
Alexey <alexeishibakov@yandex.ru>
2024-01-05 20:53:42 +0300

    homework 2

--- src/item.py
+++ src/item.py
@@ -1,27 +1,41 @@
+import csv
+
+
 class Item:
-    """
-    Класс для представления товара в магазине.
-    """
     pay_rate = 1.0
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        self.all.append(self)
 
-    def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
+    @property
+    def name(self):
+        return self.__name
 
-        :return: Общая стоимость товара.
-        """
+    @name.setter
+    def name(self, name):
+        self.__name = name[:10]
+
+    def calculate_total_price(self):
         return self.price * self.quantity
 
-    def apply_discount(self) -> None:
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
+    def apply_discount(self):
         self.price = self.price * self.pay_rate
+        return self.price
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open(
+                "C:/Users/Женя/electronics-shop-project/src/items.csv", newline=""
+        ) as csfile:
+            reader = csv.DictReader(csfile)
+            for row in reader:
+                print(row["name"], row["price"], row["quantity"])
+                list_item = cls(row["name"], row["price"], row["quantity"])
+                cls.all.append(list_item)
+
+    @staticmethod
+    def string_to_number(string):
+        return int(float(string))
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,19 +1,26 @@
 import pytest
 from src.item import Item
 
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
-
 
 @pytest.fixture
-def item():
+def item_1():
     return Item("Смартфон", 10000, 20)
 
 
-def test_calculate_total_price(item):
-    assert item.calculate_total_price() == 200000
+def test_calculate_total_price(item_1):
+    assert item_1.calculate_total_price() == 200000
 
 
-def test_apply_discount(item):
-    item.pay_rate = 0.8
-    item.apply_discount()
-    assert item.price == 8000.0
+def test_apply_discount(item_1):
+    assert item_1.apply_discount() * 0.8 == 8000.0
+
+
+def test_instantiate_from_csv(item_1):
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+
+
+def test_string_to_number(item_1):
+    assert Item.string_to_number("5") == 5
+    assert Item.string_to_number("5.0") == 5
+    assert Item.string_to_number("5.5") == 5
